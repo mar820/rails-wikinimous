13e8ab627d30cd1db5f1c0f9edec0fd9adc8bab5
Marinos Veisllari <veisllarimarinos@gmail.com>
2023-02-22 00:09:58 +0100

    Playing with MVC in rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  patch 'articles/:id', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  username = Faker::FunnyName.two_word_name
+  story = Faker::Quote.most_interesting_man_in_the_world
+  Article.create(title: username, content: story)
+end
